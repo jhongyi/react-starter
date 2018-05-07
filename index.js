597787c27a16404c368268f84c6c16a330a343fb
Jacky_JI_Chen <Jacky_JI_Chen@wistron.com>
2018-05-07 17:17:59 +0800

    chore: component propstype

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import {
   HashRouter as Router,
 } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+// import { Provider } from 'react-redux';
+// import { ConnectedRouter } from 'react-router-redux';
 import { hot } from 'react-hot-loader';
 import { history } from '~~store';
 
 import Routes from './routes';
 
 const App = () => (
-  <Router>
+  <Router history={history}>
     <Routes />
   </Router>
 );
